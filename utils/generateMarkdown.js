9751203319b7fdfa63ae876cbec2828733b6fd48
wingram1 <waingram96@gmail.com>
2022-02-12 17:49:23 -0600

    added license links

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -25,13 +25,13 @@ function renderLicenseLink(license) {
 
   switch (license) {
     case 'MIT': 
-      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+      return '[MIT](https://opensource.org/licenses/MIT)';
     case 'APACHE 2.0':
-      return'[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+      return'[APACHE 2.0](https://www.apache.org/licenses/LICENSE-2.0)';
     case 'GPL 3.0':
-      return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+      return '[GPL 3.0](https://www.gnu.org/licenses/gpl-3.0.en.html)';
     case 'BSD 3':
-      return '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
+      return '[BSD 3.0](https://opensource.org/licenses/BSD-3-Clause)';
     case 'None':
       return '';
     default: 
